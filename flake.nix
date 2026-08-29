613cace3ed9f9e2e9a6735c4338c0f98f7c0dd15
oTheAnalyst <michaelvcumbo21@proton.me>
2026-08-29 01:58:01 -0400

    init commit

---
+++ flake.nix
@@ -0,0 +1,141 @@
+{
+  description = "A demo of sqlite-web and multiple postgres services";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/x86_64-linux";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "github:juspay/services-flake";
+    northwind.url = "github:pthom/northwind_psql";
+    northwind.flake = false;
+  };
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import inputs.systems;
+      imports = [
+        inputs.process-compose-flake.flakeModule
+      ];
+      perSystem = {
+        self',
+        pkgs,
+        config,
+        lib,
+        ...
+      }: {
+        # `process-compose.foo` will add a flake package output called "foo".
+        # Therefore, this will add a default package that you can build using
+        # `nix build` and run using `nix run`.
+        process-compose."simple-example" = {config, ...}: let
+          dbName = "sample";
+        in {
+          imports = [
+            inputs.services-flake.processComposeModules.default
+          ];
+
+          services.postgres."pg1" = {
+            enable = true;
+            initialDatabases = [
+              {
+                name = dbName;
+                schemas = ["${inputs.northwind}/northwind.sql"];
+              }
+            ];
+          };
+
+          settings.processes.pgweb = let
+            pgcfg = config.services.postgres.pg1;
+          in {
+            environment.PGWEB_DATABASE_URL = pgcfg.connectionURI {inherit dbName;};
+            command = pkgs.pgweb;
+            depends_on."pg1".condition = "process_healthy";
+          };
+          settings.processes.test = {
+            command = pkgs.writeShellApplication {
+              name = "pg1-test";
+              runtimeInputs = [config.services.postgres.pg1.package];
+              text = ''
+                echo 'SELECT version();' | psql -h 127.0.0.1 ${dbName}
+              '';
+            };
+            depends_on."pg1".condition = "process_healthy";
+          };
+        };
+        packages.default = self'.packages.simple-example;
+        devShells.default = let
+          fmart =
+            pkgs.writeShellScriptBin "fmart"
+            ''pgcli -h localhost -d fmart'';
+          sendb =
+            pkgs.writeShellScriptBin "sendb"
+            ''psql -h localhost -d fmart'';
+          redev =
+            pkgs.writeShellScriptBin "redev"
+            ''rm -rf .devenv && devenv up'';
+          ingest =
+            pkgs.writeShellScriptBin "ingest"
+            ''
+              cd "$(git rev-parse --show-toplevel)" && 
+               duckdb < sql/ingestion.sql &&
+                              echo "ingestion completed!" | cowsay'';
+          etl =
+            pkgs.writeShellScriptBin "etl"
+            # bash
+            ''
+              cd "$(git rev-parse --show-toplevel)" && 
+               sendb < sql/intermediate/full_table.sql &&
+               sendb < sql/intermediate/clean_category.sql &&
+               sendb < sql/intermediate/clean_transaction.sql &&
+               sendb < sql/mart/insert_account.sql &&
+               sendb < sql/mart/insert_calender.sql &&
+               sendb < sql/mart/insert_transaction.sql &&
+               sendb < sql/mart/insert_category.sql &&
+               sendb < sql/mart/insert_fact.sql &&
+               sendb < sql/mart/view_expenditures.sql &&
+               sendb < sql/mart/view_net_income.sql &&
+               sendb < sql/mart/view_revenue.sql &&
+               sendb < sql/mart/view_transaction_type.sql &&
+                              echo "ETL completed!" | cowsay'';
+          pythonpkgs = (pkgs.python3.withPackages (ps:
+            with ps; [
+              numpy
+              dash
+              pandas
+              streamlit
+              requests
+              keyring
+            ])).override {ignoreCollisions = true;};
+        in
+          pkgs.mkShell {
+            inputsFrom = [
+              # Add the packages of the enabled services in the devShell
+              #
+              # For example: `psql` to interact with `postgres` server or `redis-cli` with `redis-server`
+              config.process-compose."simple-example".services.outputs.devShell
+            ];
+            packages = with pkgs; [
+              # Add the process-compose app in the devShell
+              sendb
+              etl
+              redev
+              fmart
+              ingest
+              cowsay
+              postgresql
+              pgcli
+              pythonpkgs
+              sqlfluff
+              duckdb
+              devenv
+              #
+              # In the devShell, run `simple-example` to run the app
+              self'.packages.simple-example
+            ];
+            shellHook = ''
+              echo "Looks like you comleted the flake services new build" |
+                cowsay
+            '';
+            nativeBuildInputs = [pkgs.just];
+          };
+      };
+    };
+}
